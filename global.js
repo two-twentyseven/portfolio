11ac2ff994f2a3d9aca79bc0b8eb99c63e5d6617
two-twentyseven <twoorthreewords@gmail.com>
2026-04-18 21:59:15 -0700

    Lab 3 Changes
    Added JS for light and dark mode, updated the navbar

---
+++ global.js
@@ -0,0 +1,65 @@
+console.log("IT'S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// Navigation
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/website/";
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/two-twentyseven', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  nav.append(a);
+}
+
+// Color scheme switcher
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+let select = document.querySelector('.color-scheme select');
+
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
+
+select.addEventListener('change', (event) => {
+  console.log('color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
