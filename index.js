4a3e69eda2f03772d2b2ddcfb4ade7bd37c30a6b
two-twentyseven <twoorthreewords@gmail.com>
2026-05-01 21:00:43 -0700

    Wrong name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
-const githubData = await fetchGitHubData('giorgianicolaou');
+const githubData = await fetchGitHubData('two-twentyseven');
 const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
