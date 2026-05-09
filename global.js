69e5fc9e5c3de84fc4ae5f407a8c8dfcfccd0554
two-twentyseven <twoorthreewords@gmail.com>
2026-05-08 19:40:35 -0700

    Navbar Lab 6

--- global.js
+++ global.js
@@ -13,6 +13,7 @@ let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'resume/', title: 'Resume' },
   { url: 'https://github.com/two-twentyseven', title: 'GitHub' },
 ];
